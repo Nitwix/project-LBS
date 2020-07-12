89d668ef38444ade736c41178bb3f361c344063e
Niels Lachat <niels.lachat@epfl.ch>
2020-07-12 20:34:26 +0200

    a good start

--- lib/widgets/AppBody.dart
+++ lib/widgets/AppBody.dart
@@ -1,12 +1,13 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 
+const _classes = ["apple", "avocado", "pear"];
+
 class AppBody extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: <Widget>[SortingClassesGrid() /*, ImageToSort()*/],
+      children: <Widget>[SortingClassesGrid() , ImageToSort()],
     );
   }
 }
@@ -20,30 +21,46 @@ class _SortingClassesGridState extends State<SortingClassesGrid> {
   static const _portraitCAC = 3;
   static const _landscapeCAC = 5;
 
-  static const _classes = ["apple", "avocado", "pear"];
-
   @override
   Widget build(BuildContext context) {
-    return OrientationBuilder(builder: (context, orientation) {
       return GridView.count(
         crossAxisCount:
-            orientation == Orientation.portrait ? _portraitCAC : _landscapeCAC,
+            MediaQuery.of(context).orientation == Orientation.portrait ? _portraitCAC : _landscapeCAC,
         children: _makeSortingClasses(),
+        physics: NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
       );
-    });
   }
 
   List<Widget> _makeSortingClasses() {
     return _classes.map((s) {
       return DragTarget(
         builder: (context, candidateData, rejectedData) {
-          return Image.file(
-            File('assets/$s/0.jpg'),
-            scale: 1,
-            repeat: ImageRepeat.noRepeat,
-          );
+          return Image.asset('assets/test_images/$s/0.jpg');
         },
       );
     }).toList();
+  }
+}
+
+class ImageToSort extends StatefulWidget {
+  @override
+  _ImageToSortState createState() => _ImageToSortState();
+}
+
+class _ImageToSortState extends State<ImageToSort> {
+  @override
+  Widget build(BuildContext context) {
+    const imgName = "assets/test_images/apple/0.jpg";
+    const imgSize = 200.0;
+    final image = Image.asset(imgName, fit: BoxFit.contain, height: imgSize, width: imgSize,);
+    return Container(
+      child: Center(
+        child: Draggable(
+          child: image,
+          feedback: image,
+        ),
+      ),
+    );
   }
 }
