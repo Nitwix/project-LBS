385174f23f64b79d98ff28dce1081d006661194c
Niels Lachat <niels.lachat@epfl.ch>
2020-07-16 22:03:38 +0200

    added gnu license to every file

--- lib/widgets/AppBody.dart
+++ lib/widgets/AppBody.dart
@@ -1,3 +1,23 @@
+// Learn By Sorting: Flutter app to help people learn by sorting.
+// Copyright (C) 2020  Niels Lachat
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// The license file can be found in the LICENSE file, at the root of the repo.
+//
+// The author can be contacted by email at nielsnfsmw@gmail.com.
+
 import 'package:flutter/material.dart';
 
 import 'package:project_lbs/backend/Metadata.dart';
@@ -64,13 +84,6 @@ class _TargetsGridState extends State<TargetsGrid> {
         },
         onWillAccept: (dragData) {
           var c = dragData.labels.contains(l.label);
-//          if(!c) {
-//            Scaffold.of(context).showSnackBar(
-//                SnackBar(
-//                  content: Text("Faux!"),
-//                  duration: Duration(microseconds: 400),
-//                ));
-//          }
           return c;
         },
         onAccept: (dragData) => Scaffold.of(context).showSnackBar(
